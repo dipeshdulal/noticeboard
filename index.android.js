157a456fb50ea22152afd8e8030dd490548a6cac
dexhrestha <dipesh.shrestha009@gmail.com>
2017-05-23 01:23:52 +0545

    Changed index for login layout

--- index.android.js
+++ index.android.js
@@ -9,47 +9,23 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
+  Image,
   View
 } from 'react-native';
 
 import * as firebase from "firebase";
+import Splash from './Splash'
+import Login from './src/components/Login/Login'
 
-export default class noticeboard extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
+export default class noticeboard extends Component{
+render(){
+  return(
+   
+      <Login/>
     );
-  }
+
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+}
 
 AppRegistry.registerComponent('noticeboard', () => noticeboard);
